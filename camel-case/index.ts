5b82954edd7deabe0e0b0c61efa740fdbb2acb35
justwink <rollawaypoint@gmail.com>
2020-12-01 16:17:10 +0800

    Add test case

--- camel-case/index.ts
+++ camel-case/index.ts
@@ -1,12 +1,8 @@
 // ts version @4.1.2
 // new feature Lowercase Uppercase
 
-export {};
-
 type CamelCase<
   S extends string
 > = S extends `${infer P1}_${infer P2}${infer P3}`
   ? `${Lowercase<P1>}${Uppercase<P2>}${CamelCase<P3>}`
   : Lowercase<S>;
-
-type Result = CamelCase<"aaaa_bb_c_ddd_E_FF">; // aaaaBbCDddEFf
