f37e4f24bc98b182d650d41703ce91b73ef7ec36
justwink <rollawaypoint@gmail.com>
2020-12-02 17:17:54 +0800

    Add currying

---
+++ currying/index.ts
@@ -0,0 +1,10 @@
+type Curry<T extends Function> = T extends (
+  p: infer P,
+  ...rest: infer Rest
+) => infer R
+  ? Rest["length"] extends 0
+    ? T
+    : (p: P) => Curry<(...rest: Rest) => R>
+  : never;
+
+declare function Currying<T extends (...args) => any>(fn: T): Curry<T>;
